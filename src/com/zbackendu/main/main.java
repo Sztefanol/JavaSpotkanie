62c602f8c8947d86be5cf90f33504a1d65627461
Sztefanol <luk266@gmail.com>
2022-03-17 21:17:20 +0100

    update 1

--- src/com/zbackendu/main/main.java
+++ src/com/zbackendu/main/main.java
@@ -1,7 +1,11 @@
 package com.zbackendu.main;
 
+import java.util.Scanner;
+
 public class main {
     public static void main(String[] args){
-        System.out.println("HELLO SEBASTIAN");
+        Scanner scanner = new Scanner(System.in);
+        byte[] input = new byte[10];
+        System.out.println(new String(input));
     }
 }
